d1ec5afc63064a25c8f865a3a63a496353ae1708
Yifan-Ivy-Hu <ashly.h.yifan@gmail.com>
2020-02-10 18:18:46 -0500

    Updated products

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -3,33 +3,33 @@ const INITIAL_STATE = {
     title: 'kitchen',
     imageUrl: 'https://i.ibb.co/2n6HyGQ/kitchen.jpg',
     id: 1,
-    linkUrl: 'shop/hats'
+    linkUrl: 'shop/kitchen'
   },
   {
     title: 'bathroom',
     imageUrl: 'https://i.ibb.co/JCpF4LT/bathroom.jpg',
     id: 2,
-    linkUrl: 'shop/jackets'
+    linkUrl: 'shop/bathroom'
   },
   {
     title: 'dining room',
     imageUrl: 'https://i.ibb.co/Y3TfMcR/diningroom.jpg',
     id: 3,
-    linkUrl: 'shop/sneakers'
+    linkUrl: 'shop/diningroom'
   },
   {
     title: 'living room',
     imageUrl: 'https://i.ibb.co/THH4SVp/livingroom.jpg',
     size: 'large',
     id: 4,
-    linkUrl: 'shop/womens'
+    linkUrl: 'shop/livingroom'
   },
   {
     title: 'bedroom',
     imageUrl: 'https://i.ibb.co/NpVxYxc/bedroom.jpg',
     size: 'large',
     id: 5,
-    linkUrl: 'shop/mens'
+    linkUrl: 'shop/bedroom'
   }]
 };
 
--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -1,246 +1,210 @@
 const SHOP_DATA = {
-  hats: {
+  kitchen: {
     id: 1,
-    title: 'Hats',
-    routeName: 'hats',
+    title: 'Kitchen',
+    routeName: 'kitchen',
     items: [
       {
         id: 1,
-        name: 'Brown Brim',
-        imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
-        price: 25
+        name: 'White Cookpot',
+        imageUrl: 'https://i.ibb.co/cwqzwJD/white-cookpot.jpg',
+        price: 50
       },
       {
         id: 2,
-        name: 'Blue Beanie',
-        imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+        name: 'Spoons',
+        imageUrl: 'https://i.ibb.co/c25TzRc/spoons.jpg',
         price: 18
       },
       {
         id: 3,
-        name: 'Brown Cowboy',
-        imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
-        price: 35
+        name: 'Kitchen Table',
+        imageUrl: 'https://i.ibb.co/CQnbvBy/kitchen-table.jpg',
+        price: 100
       },
       {
         id: 4,
-        name: 'Grey Brim',
-        imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
-        price: 25
+        name: 'Kitchen Sink',
+        imageUrl: 'https://i.ibb.co/85pTDHh/kitchen-sink.jpg',
+        price: 150
       },
       {
         id: 5,
-        name: 'Green Beanie',
-        imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
-        price: 18
+        name: 'Cookpot',
+        imageUrl: 'https://i.ibb.co/8rH1Hfy/cookpot.jpg',
+        price: 40
       },
       {
         id: 6,
-        name: 'Palm Tree Cap',
-        imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
-        price: 14
-      },
-      {
-        id: 7,
-        name: 'Red Beanie',
-        imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
-        price: 18
-      },
-      {
-        id: 8,
-        name: 'Wolf Cap',
-        imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
-        price: 14
-      },
-      {
-        id: 9,
-        name: 'Blue Snapback',
-        imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
-        price: 16
+        name: 'Cooker',
+        imageUrl: 'https://i.ibb.co/VDJGL3S/cooker.jpg',
+        price: 140
       }
     ]
   },
-  sneakers: {
+  bathroom: {
     id: 2,
-    title: 'Sneakers',
-    routeName: 'sneakers',
+    title: 'Bathroom',
+    routeName: 'bathroom',
     items: [
       {
-        id: 10,
-        name: 'Adidas NMD',
-        imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
+        id: 7,
+        name: 'Cabinet',
+        imageUrl: 'https://i.ibb.co/5KSPqRF/Cabinet.jpg',
         price: 220
       },
       {
-        id: 11,
-        name: 'Adidas Yeezy',
-        imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
+        id: 8,
+        name: 'Gray Bathtub',
+        imageUrl: 'https://i.ibb.co/RPR23Q0/gray-bathtub.jpg',
         price: 280
       },
       {
-        id: 12,
-        name: 'Black Converse',
-        imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
+        id: 9,
+        name: 'Shower',
+        imageUrl: 'https://i.ibb.co/tLQPJsJ/shower.jpg',
         price: 110
       },
       {
-        id: 13,
-        name: 'Nike White AirForce',
-        imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
+        id: 10,
+        name: 'White Bathtub',
+        imageUrl: 'https://i.ibb.co/LQhK7tz/white-bathtub.jpg',
         price: 160
       },
       {
-        id: 14,
-        name: 'Nike Red High Tops',
-        imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
+        id: 11,
+        name: 'White Bathtub 2',
+        imageUrl: 'https://i.ibb.co/3mzXw68/white-bathtub-2.jpg',
         price: 160
       },
       {
-        id: 15,
-        name: 'Nike Brown High Tops',
-        imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
+        id: 12,
+        name: 'Sink',
+        imageUrl: 'https://i.ibb.co/bFh6wfd/sink.jpg',
         price: 160
-      },
-      {
-        id: 16,
-        name: 'Air Jordan Limited',
-        imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
-        price: 190
-      },
-      {
-        id: 17,
-        name: 'Timberlands',
-        imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
-        price: 200
       }
     ]
   },
-  jackets:{
+  diningroom:{
     id: 3,
-    title: 'Jackets',
-    routeName: 'jackets',
+    title: 'Dining Room',
+    routeName: 'diningroom',
     items: [
       {
-        id: 18,
-        name: 'Black Jean Shearling',
-        imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
+        id: 13,
+        name: 'Kitchen Table 1',
+        imageUrl: 'https://i.ibb.co/Tq903JV/kitchen-table-1.jpg',
         price: 125
       },
       {
-        id: 19,
-        name: 'Blue Jean Jacket',
-        imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
+        id: 14,
+        name: 'Kitchen Table 2',
+        imageUrl: 'https://i.ibb.co/rxhghZw/kitchen-table-2.jpg',
         price: 90
       },
       {
-        id: 20,
-        name: 'Grey Jean Jacket',
-        imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
+        id: 15,
+        name: 'Kitchen Table 3',
+        imageUrl: 'https://i.ibb.co/nbgH0YS/kitchen-table-3.jpg',
         price: 90
       },
       {
-        id: 21,
-        name: 'Brown Shearling',
-        imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
+        id: 16,
+        name: 'Kitchen Table 4',
+        imageUrl: 'https://i.ibb.co/bXrR8BT/kitchen-table-4.jpg',
         price: 165
       },
       {
-        id: 22,
-        name: 'Tan Trench',
-        imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
+        id: 17,
+        name: 'Kitchen Table 5',
+        imageUrl: 'https://i.ibb.co/4FWB6T1/kitchen-table-5.jpg',
         price: 185
       }
     ]
   },
-  womens:{
+  livingroom:{
     id: 4,
-    title: 'Womens',
-    routeName: 'womens',
+    title: 'Living Room',
+    routeName: 'livingroom',
     items: [
       {
+        id: 18,
+        name: 'Sofa 1',
+        imageUrl: 'https://i.ibb.co/yphFhHF/Sofa-1.jpg',
+        price: 110
+      },
+      {
+        id: 19,
+        name: 'Sofa 2',
+        imageUrl: 'https://i.ibb.co/JrBf2V4/Sofa-2.jpg',
+        price: 100
+      },
+      {
+        id: 20,
+        name: 'Sofa 3',
+        imageUrl: 'https://i.ibb.co/M9y6HCY/Sofa-3.jpg',
+        price: 80
+      },
+      {
+        id: 21,
+        name: 'Sofa 4',
+        imageUrl: 'https://i.ibb.co/Zmqftbn/Sofa-4.jpg',
+        price: 80
+      },
+      {
+        id: 22,
+        name: 'Big Plant',
+        imageUrl: 'https://i.ibb.co/M86VGY6/big-plant.jpg',
+        price: 50
+      },
+      {
         id: 23,
-        name: 'Blue Tanktop',
-        imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
-        price: 25
-      },
-      {
-        id: 24,
-        name: 'Floral Blouse',
-        imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
-        price: 20
-      },
-      {
-        id: 25,
-        name: 'Floral Dress',
-        imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
-        price: 80
-      },
-      {
-        id: 26,
-        name: 'Red Dots Dress',
-        imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
-        price: 80
-      },
-      {
-        id: 27,
-        name: 'Striped Sweater',
-        imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
-        price: 45
-      },
-      {
-        id: 28,
-        name: 'Yellow Track Suit',
-        imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
-        price: 135
-      },
-      {
-        id: 29,
-        name: 'White Blouse',
-        imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
-        price: 20
+        name: 'Small Plant',
+        imageUrl: 'https://i.ibb.co/Jx95WDb/small-plant.jpg',
+        price: 30
       }
     ]
   },
-  mens:{
+  bedroom:{
     id: 5,
-    title: 'Mens',
-    routeName: 'mens',
+    title: 'Bedroom',
+    routeName: 'bedroom',
     items: [
       {
-        id: 30,
-        name: 'Camo Down Vest',
-        imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
-        price: 325
+        id: 24,
+        name: 'Clothes Hanger',
+        imageUrl: 'https://i.ibb.co/NV41Jm0/clothes-hanger.jpg',
+        price: 32
       },
       {
-        id: 31,
-        name: 'Floral T-shirt',
-        imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
+        id: 25,
+        name: 'Small Table',
+        imageUrl: 'https://i.ibb.co/DL775LV/small-table.jpg',
         price: 20
       },
       {
-        id: 32,
-        name: 'Black & White Longsleeve',
-        imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
-        price: 25
+        id: 26,
+        name: 'Bed 1',
+        imageUrl: 'https://i.ibb.co/nQBqc8j/bed-1.jpg',
+        price: 250
       },
       {
-        id: 33,
-        name: 'Pink T-shirt',
-        imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
-        price: 25
+        id: 27,
+        name: 'Bed 2',
+        imageUrl: 'https://i.ibb.co/4FdvgG1/bed-2.jpg',
+        price: 250
       },
       {
-        id: 34,
-        name: 'Jean Long Sleeve',
-        imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
-        price: 40
+        id: 28,
+        name: 'Bed 3',
+        imageUrl: 'https://i.ibb.co/qd8DnVS/bed-3.jpg',
+        price: 400
       },
       {
-        id: 35,
-        name: 'Burgundy T-shirt',
-        imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
-        price: 25
+        id: 29,
+        name: 'Bed 4',
+        imageUrl: 'https://i.ibb.co/kXwxLSx/bed-4.jpg',
+        price: 250
       }
     ]
   }
